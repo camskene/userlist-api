28908cd84a060ccf0938366ed9ef0040c05172c0
Cam Skene <camskene@gmail.com>
2015-08-30 22:41:49 -0400

    show user info

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -4,12 +4,15 @@ var userListData = [];
 
 $(document).ready(function() {
   populateTable();
+
+  $('#userList').on('click', '.linkshowuser', showUserInfo);
 });
 
 function populateTable() {
   var tableContent = '';
 
   $.getJSON('/users/userlist', function(data) {
+    userListData = data;
     $.each(data, function() {
       tableContent += '<tr>';
       tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
@@ -21,4 +24,27 @@ function populateTable() {
     // add to DOM
     $('#userList table tbody').html(tableContent);
   });
+}
+
+function showUserInfo(event) {
+  event.preventDefault();
+
+  // retrieve user name from rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // returns array of usernames
+  var userNameArray = userListData.map(function(arrayItem) {
+    return arrayItem.username;
+  });
+
+  // get index of thisUserName
+  var userIndex = $.inArray(thisUserName, userNameArray);
+
+  var thisUserObject = userListData[userIndex];
+
+  // populate info box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
 }
