1ed6fce51341eeadf515d0b20640e85b1dabf25e
Cam Skene <camskene@gmail.com>
2015-08-30 21:07:43 -0400

    set up userlist endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+/* global require, module */
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*
+ * GET userlist
+ * The purpose of this code is: if you do an HTTP GET to /users/userlist,
+ * our server will return JSON that lists all of the users in the database.
+ */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({},{}, function(e, docs) {
+    res.json(docs);
+  });
 });
 
 module.exports = router;
